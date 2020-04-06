b5ee1d5daa13843e6c72212a3959277a0417645f
ahelal <ahelal@users.noreply.github.com>
2020-04-06 20:41:43 +0200

    use systemd

--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -ex
-
-python3 ./main.py >> /tmp/flask.log 2>&1  &
--- setup.sh
+++ setup.sh
@@ -3,3 +3,15 @@ set -ex
 
 sudo apt-get update
 sudo apt install -y python3-flask
+
+if id -u slow > /dev/null 2>&1; then
+    echo "No need add slow user"
+else
+    sudo  useradd slow
+fi
+
+sudo  chown -R slow /opt/slowflask/
+
+sudo cp unit.service /etc/systemd/system
+sudo systemctl daemon-reload
+sudo systemctl enable slow
