a75953a3e0df6aa1836052aba56d936b39a6898c
ahelal <ahelal@users.noreply.github.com>
2020-05-13 16:57:57 +0200

    new setup

--- backend/setup.sh
+++ backend/setup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Assume that clone happened in /opt/slowflask
 base_dir="/opt/slowflask/"
 slow_user="slow"
-DB_HOST=${1-"http://localhost:8888"}
+
 sudo apt-get update
 sudo apt install -y python3-flask
 
@@ -15,8 +15,7 @@ else
 fi
 sudo chown -R "${slow_user}" "${base_dir}"
 
-sed -i -e 's/@DB_HOST/'"$DB_HOST"'/'
-sudo cp "${base_dir}/pickup/slow.service" /etc/systemd/system
+sudo cp "${base_dir}/pickup/backend.service" /etc/systemd/system
 sudo systemctl daemon-reload
-sudo systemctl enable slow
-sudo systemctl start slow
+sudo systemctl enable backend
+sudo systemctl start backend
